f39cd1b78673720375d06d376e0e8a7014a3a873
TemiloluwaPraise <temiloluwapraise06@gmail.com>
2023-03-14 09:55:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import UserList from './userList';
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <UserList />
+
+    </div>
+  );
+}
+
+export default App;
